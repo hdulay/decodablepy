ba684979a151fef9cd2296b0025d9f089182ba2b
Hubert Dulay <hubert.dulay@gmail.com>
2022-07-01 11:03:22 -0400

    build

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as readme_file:
 requirements = ["requests>=2"]
 
 setup(
-    name="decoderpy",
+    name="decodablepy",
     version="0.0.1",
     author="Hubert Dulay",
     author_email="hubert.dulay@gmail.com",
     description="a client for decodable.co",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/hdulay/decoderpy/",
+    url="https://github.com/hdulay/decodablepy/",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: Apache License V2",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
